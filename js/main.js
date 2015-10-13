9c161bf4dccd61e00e65684091ac4956205476b6
承风 <libiao.lb@taobao.com>
2015-10-14 01:45:56 +0800

    create basic pc view

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var imgList = [
+    'http://g.picphotos.baidu.com/album/s%3D1000%3Bq%3D90/sign=1275f80d58df8db1b82e78643913e625/a8014c086e061d95446ecead7df40ad162d9ca7f.jpg',
+    'http://g.picphotos.baidu.com/album/s%3D1000%3Bq%3D90/sign=ad138267d362853596e0d621a0df4db7/728da9773912b31b861235bc8018367adab4e12d.jpg'
+];
+//第二张人靠左要旋转下
