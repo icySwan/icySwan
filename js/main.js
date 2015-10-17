00ba9161308314a282d042bae9a49a836b944544
承风 <libiao.lb@taobao.com>
2015-10-18 01:01:07 +0800

    finished gallery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,153 @@
-var imgList = [
-    'http://g.picphotos.baidu.com/album/s%3D1000%3Bq%3D90/sign=1275f80d58df8db1b82e78643913e625/a8014c086e061d95446ecead7df40ad162d9ca7f.jpg',
-    'http://g.picphotos.baidu.com/album/s%3D1000%3Bq%3D90/sign=ad138267d362853596e0d621a0df4db7/728da9773912b31b861235bc8018367adab4e12d.jpg'
-];
-//第二张人靠左要旋转下
+var gallery = {
+    201504043: [
+        'http://i13.tietuku.com/5c4e74388c9c98b6.jpg',
+        'http://i13.tietuku.com/c64fda3f86281cd1.jpg',
+        'http://i13.tietuku.com/bd06a8f96834d9eb.jpg',
+        'http://i13.tietuku.com/e61f84072a80609b.jpg',
+        'http://i13.tietuku.com/bac667ff188573c2.jpg',
+        'http://i13.tietuku.com/8c9aaa3bdb266280.jpg',
+        'http://i13.tietuku.com/db6c5b45a8205656.jpg'
+    ],
+    201505: [
+        'http://i13.tietuku.com/1a7a1ed9023fbfe8.jpg',
+        'http://i13.tietuku.com/6ae898d9a7ac38ec.jpg',
+        'http://i13.tietuku.com/5923afdc81ccd281.jpg',
+        'http://i13.tietuku.com/12a7c68386f8001c.jpg',
+        'http://i13.tietuku.com/c59ff9f5c303d997.jpg',
+        'http://i13.tietuku.com/a6e1b5f070a0027b.jpg',
+        'http://i13.tietuku.com/fd8d3d5def4d3a51.jpg',
+        'http://i13.tietuku.com/40808db1479180f7.jpg',
+        'http://i13.tietuku.com/cdbb8623bc504c11.jpg',
+        'http://i13.tietuku.com/b9a553dc033fd2a8.jpg',
+        'http://i13.tietuku.com/e9ed40ec33e141ee.jpg',
+        'http://i13.tietuku.com/a779ea70c7666fab.jpg',
+        'http://i13.tietuku.com/56b6f55f110159f0.jpg',
+        'http://i13.tietuku.com/54583ffebdfdff50.jpg',
+        'http://i13.tietuku.com/3f91337d03db2241.jpg',
+        'http://i13.tietuku.com/c84093fe38c3222b.jpg',
+        'http://i13.tietuku.com/7f19f65ea3b18f5f.jpg',
+        'http://i13.tietuku.com/2936a9e8dc89db15.jpg',
+        'http://i13.tietuku.com/123366d7e43d60fd.jpg',
+        'http://i13.tietuku.com/47d727d8a5c11574.jpg',
+        'http://i13.tietuku.com/48c1dbb0f65f27c2.jpg',
+        'http://i13.tietuku.com/a9d6e76fef9c8888.jpg',
+        'http://i13.tietuku.com/b516cb246559b0d6.jpg',
+        'http://i13.tietuku.com/bcb732d8fd1f6fff.jpg',
+        'http://i13.tietuku.com/e5e01229b9989a3b.jpg',
+        'http://i13.tietuku.com/497b332289f91c64.jpg',
+        'http://i13.tietuku.com/d06d9c608c43ec47.jpg',
+        'http://i13.tietuku.com/9eed2182cce500ff.jpg',
+        'http://i13.tietuku.com/5c2c55891c80240d.jpg',
+        'http://i13.tietuku.com/5452523f0ade73bc.jpg',
+        'http://i13.tietuku.com/12e33c892d54a5f0.jpg',
+        'http://i13.tietuku.com/cd30c8ea409d02a8.jpg',
+        'http://i13.tietuku.com/0fa80047d6a5a5ce.jpg',
+        'http://i13.tietuku.com/c15904aef8e6f9a3.jpg',
+        'http://i13.tietuku.com/a0c5918a25031f0d.jpg',
+        'http://i13.tietuku.com/139ab62f0f150205.jpg',
+        'http://i13.tietuku.com/8a9abd819d4e1efd.jpg',
+        'http://i13.tietuku.com/774efb6307592190.jpg',
+        'http://i13.tietuku.com/6c57c3c2ee593ceb.jpg',
+        'http://i13.tietuku.com/09211849cf2bd73a.jpg',
+        'http://i13.tietuku.com/1576f23d3e3018d4.jpg',
+        'http://i13.tietuku.com/61da683b57ad3647.jpg',
+        'http://i13.tietuku.com/bed694e487a19b13.jpg'
+    ],
+    20150607: [
+        'http://i13.tietuku.com/bcfe0816b1ddb998.jpg',
+        'http://i13.tietuku.com/bbb37a5dd81a8fd3.jpg',
+        'http://i13.tietuku.com/004c0e00b1f6483b.jpg',
+        'http://i13.tietuku.com/e715ab2f876c66a5.jpg',
+        'http://i13.tietuku.com/65fe7902aa86ecd6.jpg',
+        'http://i13.tietuku.com/2951f2fb262b0e55.jpg',
+        'http://i13.tietuku.com/a9ee7624a35f6330.jpg',
+        'http://i13.tietuku.com/aaf342cee44e22c5.jpg',
+        'http://i13.tietuku.com/ff2c1277c95df9c6.jpg',
+        'http://i13.tietuku.com/e4e223cbbd29c0e3.jpg',
+        'http://i13.tietuku.com/46b8d47830dc9f76.jpg',
+        'http://i13.tietuku.com/06116e49e4a245d8.jpg',
+        'http://i13.tietuku.com/3b007062981a81d2.jpg',
+        'http://i13.tietuku.com/7bbd94eced868182.jpg',
+        'http://i13.tietuku.com/eb403faa261a6c0a.jpg',
+        'http://i13.tietuku.com/54c4c5780b123f26.jpg',
+        'http://i13.tietuku.com/f77ad535967c2b25.jpg',
+        'http://i13.tietuku.com/05b5504fd2dd9a2a.jpg',
+        'http://i13.tietuku.com/da856db270826131.jpg',
+        'http://i13.tietuku.com/0186dac72525559d.jpg',
+        'http://i13.tietuku.com/de44d0714d8754ac.jpg',
+        'http://i13.tietuku.com/4f41303a4ea82de2.jpg',
+        'http://i13.tietuku.com/abc19ea9294c7257.jpg',
+        'http://i13.tietuku.com/6714a2ecdc102763.jpg',
+        'http://i13.tietuku.com/9582c15bd4539370.jpg',
+        'http://i13.tietuku.com/cf81897959a834a7.jpg',
+        'http://i13.tietuku.com/f6254aaca08dc85e.jpg',
+        'http://i13.tietuku.com/2a52611b15b1fbf0.jpg'
+    ],
+    201508: [
+        'http://i13.tietuku.com/970758a846ec0118.jpg',
+        'http://i13.tietuku.com/0ff5a2ac83fc86b2.jpg'
+    ],
+    201509: [
+        'http://i13.tietuku.com/c87b94254c0f319c.jpg',
+        'http://i13.tietuku.com/26311302edcddb9f.jpg',
+        'http://i13.tietuku.com/ec4c28d0c3cf4656.jpg',
+        'http://i13.tietuku.com/fd8b939c71636415.jpg',
+        'http://i13.tietuku.com/3591b6bc33ea1692.jpg',
+        'http://i13.tietuku.com/ba34f008ddf2a905.jpg',
+        'http://i13.tietuku.com/bb03f7abd8eb2a9a.jpg',
+        'http://i13.tietuku.com/ad9efa28a9253f8f.jpg',
+        'http://i13.tietuku.com/6d7f08f590090a44.jpg',
+        'http://i13.tietuku.com/1ef07903d485d032.jpg',
+        'http://i13.tietuku.com/0bba6105056f01e7.jpg',
+        'http://i13.tietuku.com/869ef3c9b266830d.jpg',
+        'http://i13.tietuku.com/b657b96bb4ed5d56.jpg',
+        'http://i13.tietuku.com/3e3e8fe02ba289ae.jpg',
+        'http://i13.tietuku.com/c6d6cfde911ad19f.jpg',
+        'http://i13.tietuku.com/e182609c1b80ec6d.jpg',
+        'http://i13.tietuku.com/38eff0428268606c.jpg',
+        'http://i13.tietuku.com/fda86ccac1fc11d6.jpg',
+        'http://i13.tietuku.com/e03bc6b1e0e3cbc0.jpg',
+        'http://i13.tietuku.com/0882f330495b12e3.jpg',
+        'http://i13.tietuku.com/bad8ad86a215ff6f.jpg',
+        'http://i13.tietuku.com/7f76c3568ab752a5.jpg',
+        'http://i13.tietuku.com/88d98c48356e74d1.jpg',
+        'http://i13.tietuku.com/25629d37b86944cd.jpg',
+        'http://i13.tietuku.com/641d928cefa56fbb.jpg',
+        'http://i13.tietuku.com/586d3c202c422925.jpg',
+        'http://i13.tietuku.com/52b55f74ccfd885d.jpg'
+    ],
+    201510: [
+        'http://i13.tietuku.com/a6858378e298c30f.jpg',
+        'http://i13.tietuku.com/e066b830f4ff05bc.jpg'
+    ]
+};
+var gDetails = document.querySelectorAll('.g-detail');
+var len = gDetails.length;
+for(var i =0; i<len; i++) {
+    var $curDetail = gDetails[i];
+    $curDetail.addEventListener('click', function(e) {
+        e.preventDefault();
+        var $target = e.target;
+        var content = gallery[this.getAttribute('data-id')];
+        var curIndex = this.getAttribute('data-index');
+        if(typeof curIndex === 'undefined' || null === curIndex) {
+            curIndex = -1;
+        }
+        curIndex = parseInt(curIndex, 10);
+        var isLeft = !$target.classList.contains('after');
+        if(isLeft) {
+            curIndex -= 1;
+            if(curIndex < 0) {
+                curIndex = content.length - 1;
+            }
+        } else {
+            curIndex += 1;
+            if(curIndex >= content.length) {
+                curIndex = 0;
+            }
+        }
+        var $img = this.querySelector('img');
+        $img.src = content[curIndex];
+        this.setAttribute('data-index', curIndex);
+    });
+}
